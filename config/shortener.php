9217458e18e52e0476e4e4e6938c869ce9d3e8aa
Andrzej Kupczyk <kontakt@andrzejkupczyk.pl>
2020-03-04 15:03:45 +0100

    🚸 Do not require publishing config (Laravel)

--- config/shortener.php
+++ config/shortener.php
@@ -4,18 +4,18 @@ return [
     'providers' => [
         'bitly' => [
             'api_uri' => 'https://api-ssl.bitly.com/v4/',
-            'api_key' => '',
+            'api_key' => env('BITLY_API_KEY'),
             'domain' => 'bit.ly',
         ],
         'firebase' => [
             'api_uri' => 'https://firebasedynamiclinks.googleapis.com/v1/',
-            'api_key' => '',
+            'api_key' => env('FIREBASE_API_KEY'),
             'domain' => 'example.page.link',
             'unguessable' => true,
         ],
         'tinyUrl' => [
             'api_uri' => 'http://tiny-url.info/api/v1/',
-            'api_key' => '',
+            'api_key' => env('TINYURL_API_KEY'),
         ],
     ],
 ];
